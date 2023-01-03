07957b756d205f0c9ae3c548c0084483d7d9834f
Luis Gabriel Janco <janco7249@gmail.com>
2023-01-02 23:03:56 -0400

    Add changes of the project finished

--- ProyectoArchivo/Form1.Designer.cs
+++ ProyectoArchivo/Form1.Designer.cs
@@ -28,13 +28,320 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuVectorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cargarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.descargarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.grabarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.accesarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.elementosConDígitosIgualesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.elementosConDígitosDiferentesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtReason = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dígitosOrdenadosAscODescConRazónDe1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ordenarVectorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.unir2ArchOrdEnUn3erArchOrdToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox1.Location = new System.Drawing.Point(160, 81);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(90, 27);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox2.Location = new System.Drawing.Point(267, 81);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(90, 27);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox3.Location = new System.Drawing.Point(374, 81);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(90, 27);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox4.Location = new System.Drawing.Point(160, 159);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.textBox4.Size = new System.Drawing.Size(464, 75);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.WordWrap = false;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox5.Location = new System.Drawing.Point(160, 256);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.textBox5.Size = new System.Drawing.Size(464, 75);
+            this.textBox5.TabIndex = 4;
+            this.textBox5.WordWrap = false;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(157, 57);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "N. Elementos";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(264, 57);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Rango inicial";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(371, 57);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Rango final";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(159, 135);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Vector Descargado";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuVectorToolStripMenuItem,
+            this.archivoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(784, 24);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuVectorToolStripMenuItem
+            // 
+            this.menuVectorToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cargarToolStripMenuItem,
+            this.descargarToolStripMenuItem,
+            this.ordenarVectorToolStripMenuItem});
+            this.menuVectorToolStripMenuItem.Name = "menuVectorToolStripMenuItem";
+            this.menuVectorToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.menuVectorToolStripMenuItem.Text = "Vector";
+            // 
+            // cargarToolStripMenuItem
+            // 
+            this.cargarToolStripMenuItem.Name = "cargarToolStripMenuItem";
+            this.cargarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.cargarToolStripMenuItem.Text = "Cargar";
+            this.cargarToolStripMenuItem.Click += new System.EventHandler(this.cargarToolStripMenuItem_Click);
+            // 
+            // descargarToolStripMenuItem
+            // 
+            this.descargarToolStripMenuItem.Name = "descargarToolStripMenuItem";
+            this.descargarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.descargarToolStripMenuItem.Text = "Descargar";
+            this.descargarToolStripMenuItem.Click += new System.EventHandler(this.descargarToolStripMenuItem_Click);
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.grabarToolStripMenuItem,
+            this.accesarToolStripMenuItem,
+            this.elementosConDígitosIgualesToolStripMenuItem,
+            this.elementosConDígitosDiferentesToolStripMenuItem,
+            this.dígitosOrdenadosAscODescConRazónDe1ToolStripMenuItem,
+            this.unir2ArchOrdEnUn3erArchOrdToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // grabarToolStripMenuItem
+            // 
+            this.grabarToolStripMenuItem.Name = "grabarToolStripMenuItem";
+            this.grabarToolStripMenuItem.Size = new System.Drawing.Size(328, 22);
+            this.grabarToolStripMenuItem.Text = "Grabar";
+            this.grabarToolStripMenuItem.Click += new System.EventHandler(this.grabarToolStripMenuItem_Click);
+            // 
+            // accesarToolStripMenuItem
+            // 
+            this.accesarToolStripMenuItem.Name = "accesarToolStripMenuItem";
+            this.accesarToolStripMenuItem.Size = new System.Drawing.Size(328, 22);
+            this.accesarToolStripMenuItem.Text = "Accesar";
+            this.accesarToolStripMenuItem.Click += new System.EventHandler(this.accesarToolStripMenuItem_Click);
+            // 
+            // elementosConDígitosIgualesToolStripMenuItem
+            // 
+            this.elementosConDígitosIgualesToolStripMenuItem.Name = "elementosConDígitosIgualesToolStripMenuItem";
+            this.elementosConDígitosIgualesToolStripMenuItem.Size = new System.Drawing.Size(328, 22);
+            this.elementosConDígitosIgualesToolStripMenuItem.Text = "1. Elementos con dígitos iguales";
+            this.elementosConDígitosIgualesToolStripMenuItem.Click += new System.EventHandler(this.elementosConDígitosIgualesToolStripMenuItem_Click);
+            // 
+            // elementosConDígitosDiferentesToolStripMenuItem
+            // 
+            this.elementosConDígitosDiferentesToolStripMenuItem.Name = "elementosConDígitosDiferentesToolStripMenuItem";
+            this.elementosConDígitosDiferentesToolStripMenuItem.Size = new System.Drawing.Size(328, 22);
+            this.elementosConDígitosDiferentesToolStripMenuItem.Text = "2.  Elementos con dígitos diferentes.";
+            this.elementosConDígitosDiferentesToolStripMenuItem.Click += new System.EventHandler(this.elementosConDígitosDiferentesToolStripMenuItem_Click);
+            // 
+            // txtReason
+            // 
+            this.txtReason.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtReason.Location = new System.Drawing.Point(534, 81);
+            this.txtReason.Margin = new System.Windows.Forms.Padding(2);
+            this.txtReason.Name = "txtReason";
+            this.txtReason.Size = new System.Drawing.Size(90, 27);
+            this.txtReason.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(531, 57);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Razón (Ejerc. 3)";
+            // 
+            // dígitosOrdenadosAscODescConRazónDe1ToolStripMenuItem
+            // 
+            this.dígitosOrdenadosAscODescConRazónDe1ToolStripMenuItem.Name = "dígitosOrdenadosAscODescConRazónDe1ToolStripMenuItem";
+            this.dígitosOrdenadosAscODescConRazónDe1ToolStripMenuItem.Size = new System.Drawing.Size(328, 22);
+            this.dígitosOrdenadosAscODescConRazónDe1ToolStripMenuItem.Text = "3. Dígitos ordenados asc. o desc. con razón de 1.";
+            this.dígitosOrdenadosAscODescConRazónDe1ToolStripMenuItem.Click += new System.EventHandler(this.dígitosOrdenadosAscODescConRazónDe1ToolStripMenuItem_Click);
+            // 
+            // ordenarVectorToolStripMenuItem
+            // 
+            this.ordenarVectorToolStripMenuItem.Name = "ordenarVectorToolStripMenuItem";
+            this.ordenarVectorToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ordenarVectorToolStripMenuItem.Text = "Ordenar Vector";
+            this.ordenarVectorToolStripMenuItem.Click += new System.EventHandler(this.ordenarVectorToolStripMenuItem_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox6.Location = new System.Drawing.Point(160, 353);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.textBox6.Size = new System.Drawing.Size(464, 75);
+            this.textBox6.TabIndex = 12;
+            this.textBox6.WordWrap = false;
+            // 
+            // unir2ArchOrdEnUn3erArchOrdToolStripMenuItem
+            // 
+            this.unir2ArchOrdEnUn3erArchOrdToolStripMenuItem.Name = "unir2ArchOrdEnUn3erArchOrdToolStripMenuItem";
+            this.unir2ArchOrdEnUn3erArchOrdToolStripMenuItem.Size = new System.Drawing.Size(328, 22);
+            this.unir2ArchOrdEnUn3erArchOrdToolStripMenuItem.Text = "4. Unir 2 Arch. Ord. en un 3er Arch. Ord.";
+            this.unir2ArchOrdEnUn3erArchOrdToolStripMenuItem.Click += new System.EventHandler(this.unir2ArchOrdEnUn3erArchOrdToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(784, 485);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtReason);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.MinimumSize = new System.Drawing.Size(800, 524);
+            this.Name = "Form1";
+            this.Text = "Proyecto de Archivos Secuenciales";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuVectorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cargarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem descargarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem grabarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem accesarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem elementosConDígitosIgualesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem elementosConDígitosDiferentesToolStripMenuItem;
+        private System.Windows.Forms.TextBox txtReason;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ToolStripMenuItem dígitosOrdenadosAscODescConRazónDe1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ordenarVectorToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.ToolStripMenuItem unir2ArchOrdEnUn3erArchOrdToolStripMenuItem;
     }
 }
 
--- ProyectoArchivo/Form1.cs
+++ ProyectoArchivo/Form1.cs
@@ -12,9 +12,172 @@ namespace ProyectoArchivo
 {
     public partial class Form1 : Form
     {
+        Vector v1, v2;
+        FileSec F1, F2, F3;
+        SaveFileDialog SaveFileDialog1, SaveFileDialog2;
+        OpenFileDialog OpenFileDialog1, OpenFileDialog2;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            v1 = new Vector(); v2 = new Vector();
+            F1 = new FileSec(); F2 = new FileSec(); F3 = new FileSec();
+            SaveFileDialog1 = new SaveFileDialog(); OpenFileDialog1 = new OpenFileDialog();
+            SaveFileDialog2 = new SaveFileDialog(); OpenFileDialog2 = new OpenFileDialog();
+        }
+
+        private void cargarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "") throw new ArgumentException("Datos incorrectos o faltantes.");
+                v1.SetNumbers(int.Parse(textBox1.Text), int.Parse(textBox2.Text), int.Parse(textBox3.Text));
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message, "Cargar.");
+            }
+        }
+
+        private void descargarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox4.Text = v1.GetNumbers();
+        }
+
+        private void grabarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog1.ShowDialog();
+            if (SaveFileDialog1.FileName.Length > 0)
+                v1.Grabar(SaveFileDialog1.FileName, ref F1);
+            else
+                MessageBox.Show("No has creado ningún archivo para grabar.", "Grabar", MessageBoxButtons.OK);
+        }
+
+        private void accesarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog1.ShowDialog();
+            if (OpenFileDialog1.FileName.Length > 0)
+                v1.Read(OpenFileDialog1.FileName, ref F1);
+            else
+                MessageBox.Show("No has seleccionado ningún archivo para leer.", "Accesar", MessageBoxButtons.OK);
+        }
+
+        private void elementosConDígitosIgualesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog2.ShowDialog();
+            if (SaveFileDialog2.FileName.Length > 0)
+            {
+                F1.Exercise1(SaveFileDialog2.FileName, ref F2);
+                v1.Read(SaveFileDialog2.FileName, ref F2);
+                textBox5.Text = v1.GetNumbers();
+            }
+            else
+                MessageBox.Show("No has creado ningún archivo para Grabar los elementos con digitos iguales.", "Ejercicio 1", MessageBoxButtons.OK);
+        }
+
+        private void elementosConDígitosDiferentesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog2.ShowDialog();
+            if (SaveFileDialog2.FileName.Length > 0)
+            {
+                F1.Exercise2(SaveFileDialog2.FileName, ref F2);
+                v1.Read(SaveFileDialog2.FileName, ref F2);
+                textBox5.Text = v1.GetNumbers();
+            }
+            else
+                MessageBox.Show("No has creado ningún archivo para Grabar los elementos con digitos diferentes.", "Ejercicio 2", MessageBoxButtons.OK);
+        }
+
+        private void dígitosOrdenadosAscODescConRazónDe1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SaveFileDialog2.ShowDialog();
+                if (SaveFileDialog2.FileName.Length > 0)
+                {
+                    if (txtReason.Text == "") throw new ArgumentException("Debe especificar una razón para verificar el orden Asc. o Desc. de los dígitos.");
+                    F1.Exercise3(SaveFileDialog2.FileName, ref F2, int.Parse(txtReason.Text));
+                    v1.Read(SaveFileDialog2.FileName, ref F2);
+                    textBox5.Text = v1.GetNumbers();
+                }
+                else
+                    MessageBox.Show("No has creado ningún archivo para Grabar los elementos con digitos ordenados.", "Ejercicio 3", MessageBoxButtons.OK);
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message, "Ejercicio 3");
+            }
+        }
+
+        private void ordenarVectorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            v1.SortVector();
+            textBox4.Text = v1.GetNumbers();
+        }
+
+        private void unir2ArchOrdEnUn3erArchOrdToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string cancelMsg = "Has cancelado la operación del ejercicio 4.";
+                // First File
+                DialogResult dialogResult1 = MessageBox.Show("Leer el primer archivo ordenado", "Leer 1", MessageBoxButtons.YesNo);
+                if (dialogResult1 == DialogResult.Yes)
+                {
+                    OpenFileDialog1.ShowDialog();
+                    if (OpenFileDialog1.FileName.Length == 0)
+                        throw new ArgumentException("No has seleccionado ningún archivo. " + cancelMsg);
+
+                    v1.Read(OpenFileDialog1.FileName, ref F3);
+                    if (!v1.IsOrder()) throw new ArgumentException("Debes ordenar el archivo 1 que has seleccionado. " + cancelMsg);
+                }
+                else if (dialogResult1 == DialogResult.No)
+                    throw new ArgumentException(cancelMsg);
+
+                // Second File
+                DialogResult dialogResult2 = MessageBox.Show("Leer el segundo archivo ordenado", "Leer 2", MessageBoxButtons.YesNo);
+                if (dialogResult2 == DialogResult.Yes)
+                {
+                    OpenFileDialog2.ShowDialog();
+                    if (OpenFileDialog2.FileName.Length == 0)
+                        throw new ArgumentException("No has seleccionado ningún archivo. " + cancelMsg);
+
+                    v1.Read(OpenFileDialog2.FileName, ref F3);
+                    if (!v1.IsOrder()) throw new ArgumentException("Debes ordenar el archivo 2 que has seleccionado. " + cancelMsg);
+                }
+                else if (dialogResult2 == DialogResult.No)
+                    throw new ArgumentException(cancelMsg);
+
+                // Third File
+                DialogResult dialogResult3 = MessageBox.Show("Crear el archivo resultado (ordenado) para la unión de dos archivos ordenados", "Crear archivo", MessageBoxButtons.YesNo);
+                if (dialogResult3 == DialogResult.Yes)
+                {
+                    SaveFileDialog1.ShowDialog();
+                    if (SaveFileDialog1.FileName.Length == 0)
+                        throw new ArgumentException("No has creado ningún archivo. " + cancelMsg);
+                }
+                else if (dialogResult3 == DialogResult.No)
+                    throw new ArgumentException(cancelMsg);
+
+                F1.Exercise4(OpenFileDialog1.FileName, OpenFileDialog2.FileName, SaveFileDialog1.FileName, ref F2, ref F3);
+
+                v1.Read(SaveFileDialog1.FileName, ref F3);
+                textBox6.Text = v1.GetNumbers();
+
+                v1.Read(OpenFileDialog2.FileName, ref F3);
+                textBox5.Text = v1.GetNumbers();
+
+                v1.Read(OpenFileDialog1.FileName, ref F3);
+                textBox4.Text = v1.GetNumbers();
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message, "Ejercicio 4", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
         }
     }
 }
